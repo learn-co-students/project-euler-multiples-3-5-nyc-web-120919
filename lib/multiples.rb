7a58aaa7983c191e4838f6f49c49755b29b73bd1
flatironschool <danliu277@gmail.com>
2020-08-30 22:04:04 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,8 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+    return (1...limit).filter{|num| num % 3 == 0 || num % 5 == 0}
+end
+  
+def sum_multiples(limit)
+    return collect_multiples(limit).reduce{|sum, n| sum + n}
+end
